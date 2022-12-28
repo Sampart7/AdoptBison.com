4d1f2b8286b44d38ee81aa6125c0f0c4e03c86f2
Michał Janaszek <michal.janaszek7@wp.pl>
2022-12-28 17:47:44 +0100

    LOGOWANIE POCZĄTEK

---
+++ src/main/resources/static/js/register.js
@@ -0,0 +1,27 @@
+const form = document.getElementById('button')
+
+form.addEventListener('click', input =>
+{
+    input.preventDefault()
+
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const repassword = document.getElementById('repassword').value;
+
+
+    if((email!="")&&(password!="")&&(repassword!="")) {
+        if(password == repassword) {
+            fetch('/user/add',
+                {
+                    method: 'POST',
+                    headers:
+                        {
+                            'Accept': 'application/json',
+                            'Content-Type': 'application/json'
+                        },
+                    body: JSON.stringify({"email": email, "password": password, "repassword": repassword})
+                })
+            // location.href = "index.html"
+        }
+    }
+})
